14324e76ddad89f52abf8a2198144e1635cd1b3d
peng49 <1219955253@qq.com>
2021-09-13 18:13:25 +0800

    update application.py

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM debian:buster-slim
 
 COPY ./wkhtmltox_0.12.6-1.buster_amd64.deb /tmp/wkhtmltox_0.12.6-1.buster_amd64.deb
 
+COPY ./src /var/www/html
+
 RUN apt update && \
     apt install -y python && \
     apt install -y /tmp/wkhtmltox_0.12.6-1.buster_amd64.deb
 
 EXPOSE 8080
+
+ENTRYPOINT python /var/www/html/application.py
--- src/application.py
+++ src/application.py
@@ -1,4 +1,8 @@
+# -*- coding:utf-8 -*-
+import base64
+import json
 import os
+import uuid
 
 import web
 
@@ -6,7 +10,12 @@ urls = (
     '/toPDF', 'ToPDF'
 )
 
+
 class ToPDF:
+    _allow_params = [
+        '',
+    ]
+
     def __init__(self):
         pass
 
@@ -14,8 +23,37 @@ class ToPDF:
         pass
 
     def POST(self):
-        command = "wkhtmltopdf {}"
-        os.system(command)
+        body = json.loads(web.data())
+
+        try:
+            filename = uuid.uuid4().hex
+            with open("/tmp/" + filename + ".html", "w+") as f:
+                f.write(body['htmlContent'])
+
+            # 接收前端传递的参数并通过参数生成命令
+            options = body['options']
+            command_option = ""
+            print options
+            for option in options:
+                command_option = command_option + " " + option['label'] + " " + option['value']
+
+            command = "wkhtmltopdf /tmp/" + filename + ".html " + command_option + " /tmp/" + filename + ".pdf"
+
+            # 执行命令生成pdf文件
+            os.system(command)
+
+            # 读取生成的pdf文件,将文件内容转为base64编码
+            with open("/tmp/" + filename + ".pdf", "rb") as f:
+                data = base64.b64encode(f.read())
+
+            # 删除生成的缓存文件
+            os.remove("/tmp/" + filename + ".html")
+            os.remove("/tmp/" + filename + ".pdf")
+
+            return json.dumps({'code': 200, 'message': 'Success', 'data': data})
+        except BaseException as e:
+            return json.dumps({'code': 500, 'message': str(e)})
+
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
